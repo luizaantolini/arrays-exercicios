38cc5eebdba655e393ba31b373551b728e1abf3d
luizaantolini <luizaantolini@gmail.com>
2026-05-27 13:17:53 -0300

    Contagem regressiva

---
+++ ex-extras01/script.js
@@ -0,0 +1,11 @@
+// desenvolva um algoritmo que mostre, com o código "alert" uma contagem regressiva de 30 até 1, marcando os números que forem divisíveis por 4, exatamente como mostrado abaixo: 30 29 [28] 27 26 25 [24] 23 22 21 [20] 19 18 17 [16] 15 14 13 [12] 11 10 9 [8] 7 6 5 [4] 3 2 [1]
+
+let contagem = "";
+for (let i = 30; i >= 1; i--) { // a contagem começa em 30 e diminui 1 por vez
+    if (i % 4 === 0) { // se o número for dividido por 4 e o resto for igual a 0, ele é divisível por 4
+        contagem += `[${i}] `; // se o número for divisível por 4, mostre ele entre colchetes
+    } else { 
+        contagem += `${i} `; // se o número não for divisível por 4, mostre ele normalmente, sem colchetes
+    }
+}
+alert(contagem); 
